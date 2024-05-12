ae5c606982924a832a79d761ff8f45581523ea5c
JanKurdila <lektorpythonav@gmail.com>
2024-05-12 07:20:23 +0200

    4) Ovládanie hlavy hada pomocou šípok

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ def update_position(snake, direction, step):
         snake = [snake[0]-step,snake[1]]
     return snake
 
+def update_direction(direction, keys):
+    if keys[pygame.K_LEFT]:
+        return 'LEFT' if direction != 'RIGHT' else direction
+    if keys[pygame.K_RIGHT]:
+        return 'RIGHT' if direction != 'LEFT' else direction
+    if keys[pygame.K_UP]:
+        return 'UP' if direction != 'DOWN' else direction
+    if keys[pygame.K_DOWN]:
+        return 'DOWN' if direction != 'UP' else direction
+    return direction
+
 if __name__ == "__main__":
     pygame.init()
     clock = pygame.time.Clock() # Objekt
@@ -28,6 +39,8 @@ if __name__ == "__main__":
                 pygame.quit() # Vypnutie pygamu
                 sys.exit() # Vypnutie celého programu
         
+        keys = pygame.key.get_pressed() # Vrati slovnik, kde je klávesy , či sú stlačné alebo nie, (True, False)
+        smer = update_direction(smer, keys)
         had = update_position(had, smer, config.VELKOST_HADA) # Mení sa pohyb hlavičky hada v závislosti, ako je nastavený defaultný smer
 
         # Vykreslenie hlavičky hadika
