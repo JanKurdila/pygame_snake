4d3cabd149a6c49c4706e61ef99bf37c2dbd8cf4
JanKurdila <lektorpythonav@gmail.com>
2024-05-12 06:53:35 +0200

    1) Vytvorenie okna hry

---
+++ Config/config.py
@@ -0,0 +1,18 @@
+# Konštanty
+
+import pygame
+
+# Veľkosť okna, kde bude naša hra prebiehať
+ROZLISENIE = (600,600)
+
+# Farby
+FARBA_POZADIA = pygame.Color(0,0,0)
+FARBA_HLAVY_HADA = pygame.Color(51, 204, 51)
+FARBA_TELO_HADA = pygame.Color(40, 100, 30)
+FARBA_JABLKA = pygame.Color(255, 0, 0)
+
+# Veľkosti
+VELKOST_HADA = VELKOST_JABLKA = 10
+
+# Frekvencia obnovovania 
+FPS = 10
---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from Config import config
+import sys
+
+if __name__ == "__main__":
+    pygame.init()
+    window = pygame.display.set_mode(config.ROZLISENIE)
+
+    while True:
+        # Ak vypnem okno, musím vypnuť pygame
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() # Vypnutie pygamu
+                sys.exit() # Vypnutie celého programu
+
+        pygame.display.update()
