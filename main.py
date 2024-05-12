9806e55ea7dc3c7f7eba76a3b2add43bb18a684a
JanKurdila <lektorpythonav@gmail.com>
2024-05-12 07:16:48 +0200

    3) Pohyb hlavičky hada podľa defaultného smeru

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import pygame
 from Config import config
 import sys
 
+def update_position(snake, direction, step):
+    if direction == "UP":
+        snake = [snake[0],snake[1]-step]
+    if direction == "DOWN":
+        snake = [snake[0],snake[1]+step]
+    if direction == "RIGHT":
+        snake = [snake[0]+step,snake[1]]
+    if direction == "LEFT":
+        snake = [snake[0]-step,snake[1]]
+    return snake
+
 if __name__ == "__main__":
     pygame.init()
     clock = pygame.time.Clock() # Objekt
     window = pygame.display.set_mode(config.ROZLISENIE)
     # Suradnica hlavy hadika 
     had = [config.ROZLISENIE[0]//2, config.ROZLISENIE[1]//2]
+    smer = "DOWN" # Definujeme defaultný smer
 
     while True:
         # Ak vypnem okno, musím vypnuť pygame
@@ -15,6 +27,8 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit() # Vypnutie pygamu
                 sys.exit() # Vypnutie celého programu
+        
+        had = update_position(had, smer, config.VELKOST_HADA) # Mení sa pohyb hlavičky hada v závislosti, ako je nastavený defaultný smer
 
         # Vykreslenie hlavičky hadika
         pygame.draw.rect(window, config.FARBA_HLAVY_HADA, pygame.Rect(had[0], had[1], config.VELKOST_HADA, config.VELKOST_HADA))
