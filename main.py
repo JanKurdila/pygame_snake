df3aacad1f81d749827e0a17f612c5ac28e86ebc
JanKurdila <lektorpythonav@gmail.com>
2024-05-12 07:26:45 +0200

    6) Generovanie a vykresľovanie jablka na náhodnej pozícii

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from Config import config
 import sys
+import random
 
 def update_position(snake, direction, step):
     if direction == "UP":
@@ -35,6 +36,11 @@ def end_game(window):
     pygame.quit()
     sys.exit()
 
+def generate_apple(game_res, snake_size):
+    x = random.choice(range(0, game_res[0]-snake_size + 1, snake_size))
+    y = random.choice(range(0, game_res[1]-snake_size + 1, snake_size))
+    return [x, y]
+
 if __name__ == "__main__":
     pygame.init()
     clock = pygame.time.Clock() # Objekt
@@ -42,6 +48,7 @@ if __name__ == "__main__":
     # Suradnica hlavy hadika 
     had = [config.ROZLISENIE[0]//2, config.ROZLISENIE[1]//2]
     smer = "DOWN" # Definujeme defaultný smer
+    jablko = generate_apple(config.ROZLISENIE, config.VELKOST_HADA)
 
     while True:
         # Ak vypnem okno, musím vypnuť pygame
@@ -61,7 +68,9 @@ if __name__ == "__main__":
         # Vykreslenie hlavičky hadika
         pygame.draw.rect(window, config.FARBA_HLAVY_HADA, pygame.Rect(had[0], had[1], config.VELKOST_HADA, config.VELKOST_HADA))
 
-        
+        # Vykreslenie jablka
+        pygame.draw.rect(window, config.FARBA_JABLKA, pygame.Rect(jablko[0], jablko[1], config.VELKOST_HADA, config.VELKOST_HADA))
+
         pygame.display.update()
 
         # Spomalenie cyklu
